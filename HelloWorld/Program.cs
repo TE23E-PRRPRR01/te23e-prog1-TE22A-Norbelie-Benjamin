d6d7b560d45b6f83aa6c052b2b4187d41381134d
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2024-08-26 14:31:56 +0200

    tränat på cw, cr och svenska tecken

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Hej, Världen!");
+Console.WriteLine("Det är min första C# kod");
+
+Console.WriteLine("Vad gillar du att göra på din fritid?");
+string fritid = Console.ReadLine();
+Console.WriteLine(fritid + ", gud vilken kul aktivitet");
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Kollade du på OS i Paris? vilken sport var din favorit?");
