0d004309b0df53f63c5cf9f94f0af0fe9788b4b0
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2024-08-28 14:42:33 +0200

    LuckText + med rolig menings skapare

---
+++ HelloWorld/Kapitel-1/Lucktext/Program.cs
@@ -0,0 +1,17 @@
+﻿// roligt fråge porgram
+
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.BackgroundColor = ConsoleColor.Gray;
+Console.Write("Pogram för att skapa en rolig mening. ");
+Console.Write("Ange ett djur i plurar form: ");
+string djur = Console.ReadLine();
+Console.Write("ange en sport: ");
+string sport = Console.ReadLine();
+Console.WriteLine(" Visste du att " + djur + " äslkar " + sport);
+string faktan = Console.ReadLine();
+Console.Write("nu vet du någon ny rolig fakta. ");
+
+
