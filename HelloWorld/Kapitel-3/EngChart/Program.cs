e493235d55ed1782aac2951edda7d5f9b365ac96
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2024-09-02 14:51:37 +0200

    tränat på if-else satser

---
+++ HelloWorld/Kapitel-3/EngChart/Program.cs
@@ -0,0 +1,67 @@
+﻿// program som simulerar kända Engineering FlowChart
+using System.Buffers;
+using System.Xml.Schema;
+
+Console.Clear();
+Console.WriteLine("välkommen till ingenjören flödesssgcema");
+
+//Visa en ASCii art
+Console.WriteLine($"""
+
+                               _
+                             _| |_
+                           /~     ~\           
+                         /'         `\      
+                        (             )    
+                        |             |    
+                        |             |  
+                        |             |
+                        |             |
+                        |             |
+                        |             |
+                        |             |
+                        |             |
+                        |             |
+                        |             |
+                        |             |
+                      (~\_           _/~)
+                      `\_ ~~-------~~ _/'
+                         ~~---------~~
+  
+""");
+Console.WriteLine("Does it move? (y/n)");
+string answer = Console.ReadLine();
+
+// kolla om svar e ja eller nej
+if (answer == "y")
+{// y = yes
+    Console.Write("should it? (y/n)");
+    answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("No problem");
+    }
+    else
+    {
+        Console.WriteLine(" Please use Duck Tape!");
+    }
+}
+else
+{// n = no
+    Console.Write("should it? (y/n)");
+    answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("Please use WD-40!");
+    }
+    else
+    {
+        Console.WriteLine("No Problem!");
+    }
+}
+
+
+
+
+
+Console.ReadKey();
---
+++ HelloWorld/Kapitel-3/InteravtivStory/Program.cs
@@ -0,0 +1,83 @@
+﻿// fråge formulär om välmående
+using System.Buffers;
+using System.Xml.Schema;
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Hej hur mår du denna underbara dag?");
+
+
+//Visa en smile
+Console.WriteLine($"""
+     ..::''''::..
+   .;''        ``;.
+  ::    ::  ::    ::
+ ::     ::  ::     ::
+ :: .:' ::  :: `:. ::
+ ::  :          :  ::
+  :: `:.      .:' ::
+   `;..``::::''..;'
+     ``::,,,,::''  
+""");
+Console.WriteLine("Mår du bra? (ja/nej)");
+string answer = Console.ReadLine();
+
+// kolla om svar e ja eller nej
+if (answer == "ja")
+{// y = yes
+    Console.Write("Perfekt inget att lösa!");
+    answer = Console.ReadLine();
+
+}
+else
+{// n = no
+    Console.Write("har du ont i huvudet? (ja/nej)");
+    answer = Console.ReadLine();
+    if (answer == "ja")
+    {
+        Console.WriteLine("Testa dricka vatten");
+    }
+    else
+    {
+        Console.WriteLine("har du ont i magen? (ja/nej)");
+        if (answer == "ja")
+        {
+            Console.WriteLine("Testa gå på toaletten eller vila");
+        }
+        else
+        {
+            Console.WriteLine("mår du illa? (ja/nej)");
+            if (answer == "ja")
+            {
+                Console.WriteLine("du bör vila och undvika skärmar ett tag");
+            }
+            else
+            {
+             Console.WriteLine("har du någon fysisk smärta? (ja/nej)");   
+             if (answer == "ja")
+             {
+                Console.WriteLine("du bör söka hjälp av en läkare eller docktor");
+             }
+             else
+             {
+                Console.WriteLine("Mår du dåligt mentalt/psykiskt? (ja/nej)");
+                if (answer == "ja")
+                {
+                    Console.WriteLine("sök hjälp av en psyolog");
+                }
+                else
+                {
+                   Console.WriteLine("jag har svårt att tro att du mår dåligt");
+         
+                }
+             }
+            }
+        }
+    }
+}
+
+
+
+
+
+Console.ReadKey();
