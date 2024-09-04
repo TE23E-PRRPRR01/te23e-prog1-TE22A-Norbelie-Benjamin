6244aecfab9795aec31f00591ed56314bde5f7f5
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2024-09-04 14:57:41 +0200

    färdig interactive story

--- HelloWorld/Kapitel-3/InteravtivStory/Program.cs
+++ HelloWorld/Kapitel-3/InteravtivStory/Program.cs
@@ -24,53 +24,63 @@ string answer = Console.ReadLine();
 
 // kolla om svar e ja eller nej
 if (answer == "ja")
-{// y = yes
+{
     Console.Write("Perfekt inget att lösa!");
     answer = Console.ReadLine();
 
 }
 else
-{// n = no
-    Console.Write("har du ont i huvudet? (ja/nej)");
+{
+    Console.WriteLine("har du ont i huvudet? (ja/nej)");
     answer = Console.ReadLine();
     if (answer == "ja")
     {
         Console.WriteLine("Testa dricka vatten");
+
     }
     else
     {
         Console.WriteLine("har du ont i magen? (ja/nej)");
+        answer = Console.ReadLine();
         if (answer == "ja")
         {
             Console.WriteLine("Testa gå på toaletten eller vila");
+
         }
         else
         {
             Console.WriteLine("mår du illa? (ja/nej)");
+            answer = Console.ReadLine();
             if (answer == "ja")
             {
                 Console.WriteLine("du bör vila och undvika skärmar ett tag");
+
             }
             else
             {
-             Console.WriteLine("har du någon fysisk smärta? (ja/nej)");   
-             if (answer == "ja")
-             {
-                Console.WriteLine("du bör söka hjälp av en läkare eller docktor");
-             }
-             else
-             {
-                Console.WriteLine("Mår du dåligt mentalt/psykiskt? (ja/nej)");
+                Console.WriteLine("har du någon fysisk smärta? (ja/nej)");
+                answer = Console.ReadLine();
                 if (answer == "ja")
                 {
-                    Console.WriteLine("sök hjälp av en psyolog");
+                    Console.WriteLine("du bör söka hjälp av en läkare eller docktor");
+
                 }
                 else
                 {
-                   Console.WriteLine("jag har svårt att tro att du mår dåligt");
-         
+                    Console.WriteLine("Mår du dåligt mentalt/psykiskt? (ja/nej)");
+                    answer = Console.ReadLine();
+                    if (answer == "ja")
+                    {
+                        Console.WriteLine("sök hjälp av en psyolog");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("jag har svårt att tro att du mår dåligt");
+
+
+                    }
                 }
-             }
             }
         }
     }
