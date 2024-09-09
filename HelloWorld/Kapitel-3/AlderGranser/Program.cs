692e12fb3b427f99e80f0cad5b94dd1768befc4a
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2024-09-09 14:50:09 +0200

    vi har tränat på heltal och konverterting med int.Parse()

---
+++ HelloWorld/Kapitel-3/AlderGranser/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.Clear();
+Console.WriteLine("Hej hur många år är du?: ");
+
+string alderText = Console.ReadLine();
+int alder = int.Parse(alderText);
+
+
+if (alder <= 5 )
+{
+    Console.WriteLine("Du är ett litet barn");
+}
+if (alder >= 6)
+{
+    Console.WriteLine("har eller pluggar i grundskolan");
+}
+if (alder >= 15)
+{
+    Console.WriteLine("Du är byxmyndig");
+}
+if (alder >= 18)
+{
+    Console.WriteLine("Du är myndig och kan rösta");
+}
+ if ( alder >= 20 )
+{
+    Console.WriteLine("Du kan även handla på systemet");
+}
+ if (alder >= 24)
+{
+Console.WriteLine("Du kan ta körkort för tung mc");
+}
+if (alder >=67 )
+{
+    Console.WriteLine("Kan du också gå i passion");
+}
+if (alder <= 83)
+{
+    Console.WriteLine("Du lever längre än den normala svensken");
+}
---
+++ HelloWorld/Kapitel-3/Gissa Talet/Program.cs
@@ -0,0 +1,31 @@
+﻿//Ett littet spel - gissa ett hemligt heltal
+using System.Linq.Expressions;
+
+Console.Clear();
+Console.WriteLine("Ett littet spel - gissa ett hemligt heltal");
+
+//slumpa ett tal
+int slumptalet = Random.Shared.Next(1, 101);
+Console.WriteLine($"Slumptalet är {slumptalet}");
+
+Console.Write("Gissa ett tall 1-100: ");
+// läs in texten, tex "100"
+string gissningText = Console.ReadLine();
+//konvertera texten till ett heltal, blir 100
+int gissning = int.Parse(gissningText);
+
+
+if (gissning == slumptalet)
+{
+    Console.WriteLine("bra du har gissat rätt!");
+}
+else if (gissning > slumptalet)
+{
+    Console.WriteLine("du har gissat för högt");
+}
+else
+{
+    Console.WriteLine("för lågt");
+}
+
+
