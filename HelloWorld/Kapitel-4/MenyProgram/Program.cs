cdba151a71a9691c57528b9c67e54781ce781912
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2024-09-23 14:49:07 +0200

    Tränat på while-loop och fli hantering

---
+++ HelloWorld/Kapitel-4/MenyProgram/Program.cs
@@ -0,0 +1,47 @@
+﻿// Litet meny program
+Console.Clear();
+Console.WriteLine("Litet meny program");
+Console.InputEncoding=System.Text.Encoding.Unicode;
+Console.OutputEncoding=System.Text.Encoding.Unicode;
+
+//variabel för valet
+string val="";
+
+//programloop som snurrar tills användaren säger avsluta
+while(true)
+{
+    //visar en meny och läser användarens val
+    Console.WriteLine("""
+    1.omvandla en text till versaler
+    2.omvandla en text till gemener
+    3.avsluta
+    Välj ett av alternativen ovan:
+    """);
+    val =Console.ReadLine();
+    
+    //hantera användarens val
+    if (val=="1")
+    {
+        Console.Write("Skriv in en text: ");
+        string texten=Console.ReadLine().ToUpper();
+        Console.WriteLine($"texten i versaler blir: {texten}");
+        
+    }
+    else if(val=="2")
+    {
+        Console.Write("Skriv in en text: ");
+        string texten=Console.ReadLine().ToLower();
+        Console.WriteLine($"texten i versaler blir: {texten}");
+        
+    }
+    else if(val=="3")
+    {
+        Console.WriteLine("Avslutar programet...");
+        break;
+    }
+    else{
+        Console.WriteLine("Ogiltigt svars alternativ");
+    }
+
+
+}
---
+++ HelloWorld/Kapitel-4/TextEditor/Program.cs
@@ -0,0 +1,46 @@
+﻿// Här är ett Text editor program
+Console.Clear();
+Console.WriteLine("Här är ett Text editor program");
+string val = "";
+string text = "";
+while (true)
+{
+
+    Console.WriteLine("""
+
+    Välj ett av följande alternativ:
+    1. Skriv till fil
+    2. Läs från fil
+    3. Avsluta programmet
+    
+    """);
+
+    Console.Write("Dit val: ");
+    text = Console.ReadLine();
+
+    if (text == "1")
+    {
+        Console.WriteLine("Skriv in en text:");
+        // Skriv text i filen
+        val = Console.ReadLine();
+        File.WriteAllText("filnamn.txt", val);
+    }
+    else if (text == "2")
+    {
+        Console.WriteLine("Text:");
+        // Läs in text från filen
+        val = File.ReadAllText("filnamn.txt");
+        Console.WriteLine(val);
+
+    }
+    else if (text == "3")
+    {
+        Console.WriteLine("Tack för idag.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Du valde inte ett giltigt alternativ.");
+    }
+}
+
