cd509b543c198d54691470abe279257cbe8bfd67
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2024-11-20 14:56:53 +0100

    jobbat med tryparse

---
+++ HelloWorld/Kapitel-4/ForBonanza/Program.cs
@@ -0,0 +1,54 @@
+﻿// For loop bonanza
+Console.Clear();
+int min=0;
+while (true)
+{
+    
+Console.WriteLine("Ange startvärde: ");
+string minText = Console.ReadLine();
+
+bool success = int.TryParse(minText, out min);
+
+if(success==true){
+    break;
+}else{
+    Console.WriteLine("Nu gick det fel");
+}
+}
+int max=0;
+while(true){
+
+Console.WriteLine("Ange slutvärde: ");
+string maxText = Console.ReadLine();
+
+bool success = int.TryParse(maxText, out max);
+
+if(success==true){
+    break;
+}else{
+    Console.WriteLine("Nu gick det fel");
+}
+}
+int summa=0;
+
+Console.Write("Jämna tal: ");
+for (int i = min; i < max+1; i++)
+{
+    summa+=i+1;
+    if(i % 2 ==0){
+Console.Write($"{i}, ");
+    }
+
+}
+
+Console.Write("\nUdda tal: ");
+for (int i = min; i < max+1; i++)
+{
+    summa+=i+1;
+    if(i % 2 !=0){
+Console.Write($"{i}, ");
+    }
+
+}
+
+Console.WriteLine($"summan är {summa}");
