ecc5f831f1511bab1bdd6e492c124b8b311a5d7f
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2024-11-27 15:02:42 +0100

    Listor mikroövning2

---
+++ HelloWorld/Kapitel-5/mikroÖvningar2/Program.cs
@@ -0,0 +1,19 @@
+﻿// 
+Console.Clear();
+Console.WriteLine("Hello, World!");
+List<string> listaVänner = ["Cal", "Daria", "Deniss", "Malijos", "Dante"];
+Console.WriteLine($"Mina vänner är {listaVänner}");
+Console.WriteLine($"Mina kompisar är är: '{string.Join("','", listaVänner)}'");
+
+List<string> listaStad = ["Stockholm", "Tallinn", "Moskva", "New Deli", "Los Angeles"];
+Console.WriteLine($"Fem bra städer: '{string.Join("','", listaStad)}'");
+
+List<string> listaÅr = ["2013", "1817", "1939", "1914", "2024"];
+Console.WriteLine($"Fem viktiga årtal är: '{string.Join("','", listaÅr)}'");
+
+Console.WriteLine($"Jag har en namnlista med {listaVänner.Count}");
+Console.WriteLine("Första Namnet: " + listaVänner.First());
+Console.WriteLine("Sista Namnet: " + listaVänner.Last());
+
+List<string> listadryck = ["absoult", "cola", "redbull", "fanta", "brunnsvatten"];
+
