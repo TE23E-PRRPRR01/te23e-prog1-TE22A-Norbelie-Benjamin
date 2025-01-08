25bf27b7174c7fab5dba4bde16652bc942cf65dd
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2025-01-08 15:03:22 +0100

    static void och metoder

---
+++ HelloWorld/Kapitel-6/EnMassaMetoder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+Console.Clear();
+
+Säghejnamn("Benji");
+
+talIKvadrat(4);
+
+
+static void Säghejnamn(string namn)
+{
+    Console.WriteLine($"Hej på dig {namn}!");
+}
+static void talIKvadrat(int nummer)
+{
+Console.WriteLine($"{nummer} är {nummer*nummer} i kvadrat");
+}
---
+++ HelloWorld/Kapitel-6/Metoderintro/Program.cs
@@ -0,0 +1,34 @@
+﻿// 
+Console.Clear();
+
+
+
+//använd metoden
+SKIT();
+
+//namn metod
+RegistreraNamn();
+
+/// <summary>
+/// metod som säger tjena
+/// </summary>
+
+static void BS()
+{
+    Console.WriteLine("Tjena");
+}
+static void SKIT()
+{
+    Console.WriteLine("""
+    1- Lägg till uppgift
+    2- ta bort uppgift
+    3- skriv ut alla uppgift
+    4-avslut
+    """);
+}
+static void RegistreraNamn()
+{
+Console.WriteLine("Ange namn: ");
+string namn =Console.ReadLine();
+Console.WriteLine($"Tjena, {(namn)}");
+}
