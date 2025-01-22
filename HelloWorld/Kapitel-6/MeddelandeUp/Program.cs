4edd9c6b95da27662243654c935d0c3561f522f6
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2025-01-22 15:05:51 +0100

    Tränat på metoder och Topdown- strategin

---
+++ HelloWorld/Kapitel-6/MeddelandeUp/Program.cs
@@ -0,0 +1,91 @@
+﻿/****************************************************************************
+*                                 M A I N                                   *
+****************************************************************************/
+Console.Clear();
+string filnamn = "meddelande.txt";
+VisaPresentation();
+
+while (true)
+{
+    VisaMeny();
+    string val = Console.ReadLine();
+    if (val == "1")
+    {
+        Sparameddelande(filnamn);
+    }
+    else if (val == "2")
+    {
+
+        LäsMeddelande(filnamn);
+
+    }
+    else if (val == "3")
+    {
+File.Delete("meddelande.txt");
+
+    }
+    else if (val == "4")
+    {
+
+Console.WriteLine("Inget mer tack!");
+Thread.Sleep(3000);
+break;
+
+    }
+    else{
+        Console.WriteLine("Oops, fel!");
+    }
+
+}
+
+
+
+
+/**********************************************
+*               M E T H O D S                 *
+**********************************************/
+
+
+/// <summary>
+/// Presentation av programmet
+/// </summary>
+static void VisaPresentation()
+{
+System.Console.WriteLine("""
+Program för att spara meddelande
+""");
+}
+/// <summary>
+/// Visar programets meny alla funktioner
+/// </summary>
+static void VisaMeny()
+{
+Console.WriteLine("""
+1. spara meddelande
+2. läs meddelande
+3. ta bort meddelande
+4. avsluta program
+""");
+}
+
+/// <summary>
+/// sparar användarens meddelande
+/// </summary>
+static void Sparameddelande(String filen)
+{
+System.Console.WriteLine("Ange ett meddelande: ");
+string meddelande =Console.ReadLine();
+File.WriteAllText("filen", meddelande);
+System.Console.WriteLine("Meddelande sparat");
+}
+
+/// <summary>
+/// läser ut använderns meddelande
+/// </summary>
+static void LäsMeddelande(string filen)
+{
+string meddelande = File.ReadAllText("filen");
+Console.WriteLine("Ditt senaste meddelande: ");
+System.Console.WriteLine(meddelande);
+System.Console.WriteLine("---------------");
+}
---
+++ HelloWorld/Kapitel-6/Miniräknare/Program.cs
@@ -0,0 +1,70 @@
+﻿/**********************************************
+*                   M A I N                   *
+**********************************************/
+Console.Clear();
+
+
+while (true)
+{
+    Presentation();
+    string val = Console.ReadLine();
+    if (val == "1")
+    {
+        Addition();
+    }else if (val == "2")
+    {
+        Subtraktion();
+    }
+    else if (val == "3")
+    {
+        Multiplikation();
+    }
+    else if (val == "4")
+    {
+        Division();
+    }
+    else if (val == "5")
+    {
+        break;
+    }
+}
+
+
+/**********************************************
+*               M E T H O D S                 *
+**********************************************/
+static void Presentation()
+{
+    System.Console.WriteLine("Välj ett av följande alternativ");
+    System.Console.WriteLine("""
+        1. Addition
+        2. Subtraktion
+        3. Multiplikation
+        4. Division
+        5. Avsluta programmet
+   """);
+   
+}
+
+static void Addition()
+{
+System.Console.WriteLine("Välj två tal som ska adderas");
+System.Console.WriteLine("Tal1: ");
+float Tal1 = Console.ReadLine();
+
+
+System.Console.WriteLine("Tal1: ");
+
+}
+static void Subtraktion()
+{
+
+}
+static void Multiplikation()
+{
+
+}
+static void Division()
+{
+
+}
